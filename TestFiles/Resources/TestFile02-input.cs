1808a5ab804669d10c073a6ad99ae0631acbcef4
Greg Smith <smr.kyu@gmail.com>
2013-09-14 13:28:03 -0500

    Simplified test case some more

--- TestFiles/Resources/TestFile02-input.cs
+++ TestFiles/Resources/TestFile02-input.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace TestFile
 {
 	public class TestClass02
 	{
-		public System.Collections.Generic.IList<System.Collections.Generic.IList<string>> MakeDeepCollection(System.Collections.Generic.IList<string> firstElementInDeepCollection, System.Collections.Generic.IList<string> secondElementInDeepCollection, System.Collections.Generic.IList<string> thirdElementInDeepCollection) {
+		public IList<IList<string>> MakeDeepCollection(IList<string> firstElementInDeepCollection, IList<string> secondElementInDeepCollection, IList<string> thirdElementInDeepCollection) {
 			return null;
 		} 
 	}
